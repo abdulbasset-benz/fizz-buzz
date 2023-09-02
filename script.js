d39c5c2afbcba0f6f8177bd24aa530ccba2c0357
devnbeyond <benz.abdelbasset@gmail.com>
2023-09-02 15:30:15 +0100

    Add output table dynamically via js

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-var form = document.querySelector('form');
 var submitButton = document.querySelector('button');
-var displayTableDiv = document.querySelector('display-table');
+var currentPage = 1;
 
-
-// creating the table for output //
-
-form.addEventListener('click', function(event){
+submitButton.addEventListener('click', function(event) {
     event.preventDefault();
 
     var inputValue = document.querySelector('input').value;
 
-            // Call the fizzBuzz function to generate the result
-            const fizzBuzzResult = fizzBuzz(inputValue);
-
-            // Call the renderOutput function to display the result
-            const table = renderOutput(fizzBuzzResult);
-
-            // Clear the previous result
-            const outputDiv = document.getElementById('outputTable');
-            outputDiv.innerHTML = '';
-
-            // Append the new result to the output container
-            outputDiv.appendChild(table);
     
+    const fizzBuzzResult = fizzBuzz(inputValue);
+
+   
+    const table = renderOutput(fizzBuzzResult);
+
+    
+    const outputDiv = document.getElementById('outputTable');
+     
+    outputDiv.appendChild(table);
 });
-
-// game logic //
-
-
 
 function fizzBuzz(inputValue) {
     var fizzBuzzResult = [];
 
     for (let i = 1; i <= inputValue; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            fizzBuzzResult.push("fizzBuzz");
+            fizzBuzzResult.push("FizzBuzz");
         } else if (i % 3 === 0) {
-            fizzBuzzResult.push('fizz');
+            fizzBuzzResult.push('Fizz');
         } else if (i % 5 === 0) {
-            fizzBuzzResult.push('buzz');
+            fizzBuzzResult.push('Buzz');
         } else {
             fizzBuzzResult.push(i);
         }
@@ -47,20 +36,18 @@ function fizzBuzz(inputValue) {
     return fizzBuzzResult;
 }
 
-
-// rendering the output //
-
 function renderOutput(fizzBuzzResult) {
+    const container = document.getElementById("outputTable");
     const table = document.createElement("table");
-    const tableRow = document.createElement("tr");
 
     for (let i = 0; i < fizzBuzzResult.length; i++) {
-        const th = document.createElement("th");
-        th.textContent = fizzBuzzResult[i]; // Use fizzBuzzResult[i] to access each value
-        tableRow.appendChild(th);
+        const row = document.createElement("tr");
+        const cell = document.createElement("td");
+        cell.textContent = fizzBuzzResult[i];
+        row.appendChild(cell);
+        table.appendChild(row); 
     }
 
-    table.appendChild(tableRow);
-
-    return table; // Return the table element
+    return table;
 }
+
